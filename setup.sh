921c63e330b7b4eb129195e55d17bb201fb63172
Tom Hudson <mail@tomnomnom.com>
2012-04-12 00:45:19 +0100

    tmux config; vimux plugin

--- setup.sh
+++ setup.sh
@@ -4,4 +4,5 @@ if [ ! -e ~/.vimrc ]; then ln -s ~/etc/vimrc ~/.vimrc; fi;
 if [ ! -e ~/.ackrc ]; then ln -s ~/etc/ackrc ~/.ackrc; fi;
 if [ ! -e ~/.bashrc ]; then ln -s ~/etc/bashrc ~/.bashrc; fi;
 if [ ! -e ~/.gitconfig ]; then ln -s ~/etc/gitconfig ~/.gitconfig; fi;
+if [ ! -e ~/.tmux.conf ]; then ln -s ~/etc/tmux.conf ~/.tmux.conf; fi;
 
