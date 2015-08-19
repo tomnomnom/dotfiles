a8a850fbf6f5124a9807e0eff569930ecb1d8959
Tom Hudson <mail@tomnomnom.com>
2015-08-19 16:34:58 -0400

    Switches go install setup script to go 1.5 :)

--- go-setup.sh
+++ go-setup.sh
@@ -3,12 +3,12 @@
 # Dirty dirty dirty
 
 source .bashrc
-wget https://storage.googleapis.com/golang/go1.4.2.linux-amd64.tar.gz
-tar xvfz go1.4.2.linux-amd64.tar.gz
+wget https://storage.googleapis.com/golang/go1.5.linux-amd64.tar.gz
+tar xvfz go1.5.linux-amd64.tar.gz
 sudo rm -rf /usr/local/go
 sudo mv go /usr/local/
 
 go get golang.org/x/tools/cmd/goimports
 go get github.com/nsf/gocode
 
-rm go1.4.2.linux-amd64.tar.gz
+rm go1.5.linux-amd64.tar.gz
