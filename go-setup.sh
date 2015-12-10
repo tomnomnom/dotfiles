bf4d183ceed0e3f19764305a1483fa937a3a790b
Tom Hudson <mail@tomnomnom.com>
2015-12-10 13:49:22 -0500

    Bumps Go version to 1.5.2

--- go-setup.sh
+++ go-setup.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 # Dirty dirty dirty
+VERSION="1.5.2"
 
 source .bashrc
-wget https://storage.googleapis.com/golang/go1.5.linux-amd64.tar.gz
-tar xvfz go1.5.linux-amd64.tar.gz
+wget https://storage.googleapis.com/golang/go$VERSION.linux-amd64.tar.gz
+tar xvfz go$VERSION.linux-amd64.tar.gz
 sudo rm -rf /usr/local/go
 sudo mv go /usr/local/
 
 go get golang.org/x/tools/cmd/goimports
 go get github.com/nsf/gocode
 
-rm go1.5.linux-amd64.tar.gz
+rm go$VERSION.linux-amd64.tar.gz
